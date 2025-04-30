2f6a9bc4c425981b2580477f2ced6d68a92ae4c0
Mehul Saini <mehulsaini1237821@gmail.com>
2025-04-30 16:12:30 +0530

    Update Q-14.py

--- Q-14.py
+++ Q-14.py
@@ -1,9 +1,27 @@
-#14.Write a program to find LCM of two numbers.
+# Program to find the LCM of two numbers
 
-num=int(input("Enter a number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-lcm=0
-for i in range(2,num):
-        lcm=num//i;
-        
+n=num1
+i=2
+print("LCM of",num1,"=",end=" ")
+while(i<=n):
+        if(n%i==0):
+                print(i,end=" ")
+                n=n//i;
+        else:
+                i+=1
+                
+print()
 
+print("LCM of",num2,"=",end=" ")
+n2=num2
+i=2
+while(i<=n2):
+        if(n2%i==0):
+                print(i,end=" ") 
+                n2=n2//i 
+        else:
+                i+=1
+                
